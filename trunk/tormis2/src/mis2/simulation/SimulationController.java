1fc20fa6032c92e2d246f5f04a9de5129043e606
notargiacomo.s <notargiacomo.s@c316c57c-90a1-11dd-8d30-8f0ea4a41c47>
2008-10-02 17:47:38 +0000

    Initial import.

---
+++ trunk/tormis2/src/mis2/simulation/SimulationController.java
@@ -0,0 +1,15 @@
+package mis2.simulation;
+
+public class SimulationController {
+	
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
