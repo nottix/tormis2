5637f4560737791aeea53b0141b7177e51753b1a
gabriele.pacini@gmail.com <gabriele.pacini@gmail.com@c316c57c-90a1-11dd-8d30-8f0ea4a41c47>
2008-10-27 12:12:42 +0000

    Creazione MatrixByte v0.2

--- trunk/tormis2/src/mis2/markov/MatrixByte.java
+++ trunk/tormis2/src/mis2/markov/MatrixByte.java
@@ -36,23 +36,40 @@ public class MatrixByte {
             byte sign = 0;
             String tmp = "";
             
-            System.out.println("Double: " + numStr);
-            
             for(int k=0; k<strCrt.length; k++){
                 if(k==0){
                     sign = (strCrt[0]=='-')? (byte)1 : (byte)0;
                 }
                 if(strCrt[k] == '.'){
                     int len = ( (strCrt.length - k)>5 )? 5 : (strCrt.length - k);
-                    tmp = numStr.substring( ((sign==1)? 1 : 0), k);
 
-                    matrix[(i*this.numCols) + j] = new byte[len + 1];
-                    matrix[(i*this.numCols) + j][0] = sign;
-                    matrix[(i*this.numCols) + j][1] = Byte.valueOf(tmp);
+                    if(k>=3 && k<=4) {
+                        matrix[(i*this.numCols) + j] = new byte[len + 2];
+                        matrix[(i*this.numCols) + j][0] = sign;
+                        tmp = numStr.substring( ((sign==1)? 1 : 0), k-1);
+                        matrix[(i*this.numCols) + j][1] = Byte.valueOf(tmp);
+                        tmp = numStr.substring( k-1, k);
+                        matrix[(i*this.numCols) + j][2] = Byte.valueOf(tmp);
 
-                    for(int iNum=1; iNum<len; iNum++){
-                        tmp = numStr.substring(k+iNum, k+iNum+1);
-                        matrix[(i*this.numCols) + j][iNum + 1] = Byte.valueOf(tmp);
+                        for(int iNum=2; iNum<len; iNum++){
+                            tmp = numStr.substring(k+iNum, k+iNum+1);
+                            matrix[(i*this.numCols) + j][iNum + 2] = Byte.valueOf(tmp);
+                        }
+                    }
+                    else if(k<3) {
+                        tmp = numStr.substring( ((sign==1)? 1 : 0), k);
+                        matrix[(i*this.numCols) + j] = new byte[len + 1];
+                        matrix[(i*this.numCols) + j][0] = sign;
+                        matrix[(i*this.numCols) + j][1] = Byte.valueOf(tmp);
+
+                        for(int iNum=1; iNum<len; iNum++){
+                            tmp = numStr.substring(k+iNum, k+iNum+1);
+                            matrix[(i*this.numCols) + j][iNum + 1] = Byte.valueOf(tmp);
+                        }
+                    }
+                    else {
+                        System.out.println("Errore - bastaaa!!!\n");
+                        System.out.println(num);
                     }
                     break;
                 }
@@ -67,8 +84,11 @@ public class MatrixByte {
             double ret = 0.0;
             String retStr = "";
             
+            if(this.matrix[(i*this.numCols) + j] == null){
+                return 0.0;
+            }
+            
             for(int k=0; k<this.matrix[(i*this.numCols) + j].length; k++){
-                System.out.println(this.matrix[(i*this.numCols) + j][k]);
                 if(k==0){
                     retStr += (this.matrix[(i*this.numCols) + j][k]==1)? "-" : "";
                 }
@@ -80,11 +100,18 @@ public class MatrixByte {
                 }
             }
             
-            System.out.println("get: " + retStr);
             ret = Double.valueOf(retStr);
             retStr = null;
             
             return ret;
             
+        }
+        
+        public int numRows() {
+            return this.numRows;
+        }
+        
+        public int numColumns() {
+            return this.numCols;
         }
 }
